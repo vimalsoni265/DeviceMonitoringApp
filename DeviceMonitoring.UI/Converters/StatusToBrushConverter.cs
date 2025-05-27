8045f9d9ced8be9c1f7e2587503346580c1d2219
Vimalkumar Soni <gurusoni265@live.com>
2025-05-27 22:23:02 +0000

    Merged PR 6: Update UI to have a light weight DataTemplate for better optimization.

---
+++ DeviceMonitoring.UI/Converters/StatusToBrushConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace DeviceMonitoring.UI.Converters
+{
+    public class StatusToBrushConverter : IValueConverter
+    {
+        public Brush MonitoringBrush { get; set; } = Brushes.Green;
+        public Brush StoppedBrush { get; set; } = Brushes.Red;
+        public Brush DefaultBrush { get; set; } = Brushes.Gray;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var status = value as string;
+            return status switch
+            {
+                "Monitoring" => MonitoringBrush,
+                "Stopped" => StoppedBrush,
+                _ => DefaultBrush
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
